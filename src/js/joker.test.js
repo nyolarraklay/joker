514f65e425450ca00233503fe1b46c65e61f9db9
Ernesto Jr Fundal Osorio <114483893+nyolarraklay@users.noreply.github.com>
2023-10-27 10:13:30 +0200

    test babel

--- src/js/joker.test.js
+++ src/js/joker.test.js
@@ -1,11 +1,15 @@
-// import { dadJoke } from './joker.js'
+import { dadJoke } from './joker.js';
 
-// export async function testDadJoke () {
-//   const joke = await dadJoke()
-//   const testPasses = Boolean(joke)
-//   console.log(joke)
-//   console.log('Dad Joke Test:', testPasses ? 'PASSED' : 'FAILED')
-//   return testPasses
-// }
+async function testDadJoke() {
+  const joke = await dadJoke();
+  const testPasses = Boolean(joke);
+  console.log(joke);
+  console.log('Dad Joke Test:', testPasses ? 'PASSED' : 'FAILED');
+  return testPasses;
+}
 
-// testDadJoke()
+testDadJoke();
+
+test('It adds 1 and 2 and gets 3 as a result', () => {
+  expect(1 + 2).toEqual(3);
+});
